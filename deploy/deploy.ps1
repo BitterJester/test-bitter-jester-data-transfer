bc5f05377ed415007afacd1229843c915ccd2731
Spencer Kasper <spencer.kasper@gmail.com>
2020-02-26 12:41:35 -0600

    Learned that we need to zip each file inside this directory individually as opposed to just zipping the folder.  Can't figure out the compression stuff right now so I am just gonna comment that out for now and come back to it later.

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -1,2 +1,8 @@
-compress-archive -path '..\test-bitter-jester-data-transfer' -destinationpath '.\test-bitter-jester-data-transfer.zip' -compressionlevel optimal
+#$compress = @{
+ #   Path= ".\node_modules", ".\lambdaHandler.js", ".\writeToS3FromJotForm.js", ".\.env", ".\package.json", ".\package-lock.json"
+  #  CompressionLevel = "Fastest"
+   # DestinationPath = ".\deploy\test-bitter-jester-data-transfer.zip"
+    #}
+#Compress-Archive @compress -Force
+aws s3 cp ./deploy/test-bitter-jester-data-transfer.zip s3://bitter-jester-test/write-to-s3-from-jotform/test-bitter-jester-data-transfer.zip
 aws cloudformation deploy --template-file ./template.yml --stack-name "write-to-s3-from-jotform" --capabilities CAPABILITY_IAM
