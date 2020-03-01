c67282411cce7e48a0651bd27e7cccb1f8c389b0
Spencer Kasper <spencer.kasper@gmail.com>
2020-02-29 19:38:38 -0600

    Update to return empty string if undefined so that the front end still has a value to render

--- formatForS3.js
+++ formatForS3.js
@@ -1,5 +1,6 @@
 const BAND_NAME_QUESTION_ID = '39';
 const PRIMARY_EMAIL_QUESTION_ID = '289';
+const FIRST_CHOICE_FRIDAY_NIGHT_QUESTION = '77';
 
 const format = (s3Response) => {
     const applicationAnswersBySubmission = s3Response.map(application => {
@@ -13,7 +14,8 @@ const format = (s3Response) => {
     return applicationAnswersBySubmission.map(answersForBand => {
         return {
             bandName: getAnswerByQuestionId(answersForBand, BAND_NAME_QUESTION_ID),
-            primaryEmailAddress: getAnswerByQuestionId(answersForBand, PRIMARY_EMAIL_QUESTION_ID)
+            primaryEmailAddress: getAnswerByQuestionId(answersForBand, PRIMARY_EMAIL_QUESTION_ID),
+            firstChoiceFridayNight: getAnswerByQuestionId(answersForBand, FIRST_CHOICE_FRIDAY_NIGHT_QUESTION) || ''
         };
     });
 }
--- formatForS3.test.js
+++ formatForS3.test.js
@@ -1,28 +1,27 @@
 const formatForS3 = require('./formatForS3');
 
 describe('FormatForS3', () => {
-    const buildAnswer = (bandName, email) => {
-        return {
-            "39": {
-                name: "retypeYour",
-                answer: bandName
-            },
-            "289": {
-                name: "retypeYour289",
-                answer: email
-            }
+    const buildAnswers = (bandName, email, firstChoiceDate) => {
+        const buildSingleAnswer = (name, answer) => {
+            return {
+                name: name,
+                answer: answer
+            };
         };
-    }
-
-    const buildS3Response = (bandName, email) => {
         return {
-            answers: buildAnswer(bandName, email)
+            "39": buildSingleAnswer("retypeYour", bandName),
+            "289": buildSingleAnswer("retypeYour289", email),
+            "77": buildSingleAnswer("selectYour77", firstChoiceDate)
         };
     };
 
     const s3Response = [
-        buildS3Response("bandName", "email"),
-        buildS3Response("bandName2", "email2")
+        {
+            answers: buildAnswers("bandName", "email", "1stChoiceDate")
+        },
+        {
+            answers: buildAnswers("bandName2", "email2", undefined)
+        }
     ];
 
     const actual = formatForS3.format(s3Response);
@@ -35,11 +34,19 @@ describe('FormatForS3', () => {
         expect(actual[0].primaryEmailAddress).toEqual('email');
     });
 
+    it('should return a 1st choice date from the response', () => {
+        expect(actual[0].firstChoiceFridayNight).toEqual('1stChoiceDate');
+    });
+
     it('should return a primary email for band 2', () => {
         expect(actual[1].bandName).toEqual('bandName2');
     });
 
     it('should return a primary email for band 2', () => {
         expect(actual[1].primaryEmailAddress).toEqual('email2');
+    });
+
+    it('should return an empty string for undefined firstChoiceDate for band 2', () => {
+        expect(actual[1].firstChoiceFridayNight).toEqual('');
     });
 });
