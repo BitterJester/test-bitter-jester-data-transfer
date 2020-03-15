f044360233b964ffb605c80517351f56b2803ef9
Spencer Kasper <spencer.kasper@gmail.com>
2020-03-15 18:32:06 -0500

    Update deploy stuff so that the new files get deployed to a better named s3 path

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -4,6 +4,5 @@
    # DestinationPath = ".\deploy\test-bitter-jester-data-transfer.zip"
     #}
 #Compress-Archive @compress -Force
-$version = 1
-aws s3 cp ./deploy/test-bitter-jester-data-transfer.zip "s3://bitter-jester-test/write-to-s3-from-jotform/test-bitter-jester-data-transfer-${version}.zip"
+aws s3 cp ./deploy/test-bitter-jester-data-transfer.zip "s3://bitter-jester-test/bitter-jester-data-transfer/test-bitter-jester-data-transfer.zip"
 aws cloudformation deploy --template-file ./template.yml --stack-name "write-to-s3-from-jotform" --capabilities CAPABILITY_IAM
