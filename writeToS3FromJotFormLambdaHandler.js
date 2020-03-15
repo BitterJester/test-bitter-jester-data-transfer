9c34ac86b9d2b14a826c1d2881aa3de9dab5f82a
Spencer Kasper <spencer.kasper@gmail.com>
2020-03-15 18:23:58 -0500

    Send sns message when write to s3 from jot form is finished

--- writeToS3FromJotFormLambdaHandler.js
+++ writeToS3FromJotFormLambdaHandler.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 const writeToS3FromJotForm = require('./writeToS3FromJotForm/writeToS3FromJotForm');
+const SNSClient = require('./snsClient').SNSClient;
 
 exports.handler = async function(event) {
     await writeToS3FromJotForm.getFormSubmissions();
+    await new SNSClient().publishSNSMessage({
+        Message: 'hi',
+        TopicArn: 'arn:aws:sns:us-east-1:771384749710:BandApplicationUpdatedSnsTopic'
+    });
+
+    console.log('DONE')
 }
