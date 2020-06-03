59559befa6e0f33e4938c70baebe86d2ff35dc9d
Spencer Kasper <spencer.kasper@gmail.com>
2020-06-02 21:10:17 -0500

    * Update to handle urls better

--- getOriginalSongSubmissions/formatOriginalSongSubmissions.js
+++ getOriginalSongSubmissions/formatOriginalSongSubmissions.js
@@ -5,18 +5,24 @@ const jotformAnswerMap = {
     songName: '543',
     bandName: '39',
     primaryEmailAddress: '289',
-    songUrl: '252',
     bandPhotoUrl: '253',
     lyricsUrl: '540',
     songDescription: '539'
 };
 
 const format = async (applications) => {
+    const formatLyrics = (lyricsUrl) => {
+      const domain = 'https://www.jotform.com/uploads/BitterJester/';
+      const domainRemoved = lyricsUrl.replace(domain, '');
+        const urlParamParts = domainRemoved.split('/');
+        urlParamParts[2] = encodeURIComponent(urlParamParts[2]);
+      return `${domain}${urlParamParts.join('/')}`
+    };
+
     const extractedApplications = extractAnswersFromJotform.extractAnswersFromJotform(applications, jotformAnswerMap)
         .map(item => {
-            item.songUrl = encodeURI(item.songUrl[0]);
             item.bandPhotoUrl = encodeURI(item.bandPhotoUrl[0]);
-            item.lyricsUrl = encodeURI(item.lyricsUrl[0]);
+            item.lyricsUrl = formatLyrics(encodeURI(item.lyricsUrl[0]));
             item.songDescription = item.songDescription ? item.songDescription.replace(/&#039;/g, "'") : '';
             return item;
         });
@@ -26,12 +32,13 @@ const format = async (applications) => {
         const numberOfSongsForBand = originalSongSubmissions.originalSongs.filter(app => app.bandName === song.bandName).length;
 
         if(numberOfSongsForBand === 0){
-            song.scheduledWeek = 1;
+            song.scheduledWeek = 4;
             originalSongSubmissions.originalSongs.push(song);
         }
     });
+
     return {
-        originalSongs: extractedApplications
+        originalSongs: originalSongSubmissions.originalSongs
     };
 };
 
