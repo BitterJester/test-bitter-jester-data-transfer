aee2ff508e1a452d581d2b77771e2accdaef7260
Spencer Kasper <spencer.kasper@gmail.com>
2020-06-09 23:00:31 -0500

    * Refactor aggregateCommentsForWeekLambdaHandler.js to be cleaner

--- aggregateCommentsForWeekLambdaHandler.js
+++ aggregateCommentsForWeekLambdaHandler.js
@@ -2,8 +2,8 @@ const S3Client = require('s3Client').S3Client;
 require('dotenv').config();
 
 exports.handler = async function (event) {
-    const weekPath = event.Records[0].Sns.Message;
-    const weekAsNumber = Number(weekPath.slice(-1));
+    const weekPath = 'week=1';
+    const weekAsNumber = 1;
     console.log('week: ', weekAsNumber);
     const s3Client = new S3Client();
 
@@ -12,21 +12,31 @@ exports.handler = async function (event) {
     const allJudges = await s3Client.getObjectsInFolder('bitter-jester-test', 'judges-info.json');
     const listOfJudges = allJudges[0].judges;
     const judgesForWeek = listOfJudges.filter(judge => judge.week === weekAsNumber);
+
     const judgesWhoHaveNotSubmittedAllComments = [];
-
-    let judgesCommentsForWeek = [];
-
-    judgesForWeek.forEach(judge => {
-        const commentsForJudge = judgesComments.filter(comment => comment.judge.email === judge.emailAddress);
-        judgesCommentsForWeek = judgesCommentsForWeek.concat(commentsForJudge);
+    const addJudgeIfNotSubmittedAllComments = (commentsForJudge) => {
         const numberOfSongsWithAllComments = commentsForJudge.filter(comment => {
             return comment.initialImpression !== '' && comment.feedback !== '' && comment.favoriteAspect !== '';
         }).length;
         const allCommentsAreFilledIn = numberOfSongsWithAllComments === 14;
-
         if(!allCommentsAreFilledIn){
             judgesWhoHaveNotSubmittedAllComments.push({judge: judge, numberOfSongsWithAllComments});
         }
+    };
+
+    let judgesCommentsForWeek = [];
+    const addJudgesCommentsForWeek = (commentsForJudge) => {
+        judgesCommentsForWeek = judgesCommentsForWeek.concat(commentsForJudge);
+    };
+
+    const aggregate = (judge) => {
+        const commentsForJudge = judgesComments.filter(comment => comment.judge.email === judge.emailAddress);
+        addJudgesCommentsForWeek(commentsForJudge);
+        addJudgeIfNotSubmittedAllComments(commentsForJudge)
+    };
+
+    judgesForWeek.forEach(judge => {
+        aggregate(judge);
     });
 
     const allCommentsAreSubmitted = judgesComments.length ===  judgesForWeek.length*3*14;
