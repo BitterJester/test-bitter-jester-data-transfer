ca544304c2cfa1009a08efd121e1649dfb9f781b
spencerkasper <spener.kasper@gmail.com>
2021-06-08 14:38:20 -0500

    * This should fix the aws account stuff hopefully in circle

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,4 +2,4 @@ aws cloudformation delete-stack --stack-name bitter-jester-data-manager-etl
 rm -rf bitter-jester-data-manager-etl.zip
 zip -r bitter-jester-data-manager-etl.zip . -x '.idea/**' -x '.git/**'
 aws s3 cp ./bitter-jester-data-manager-etl.zip "s3://bitter-jester-test/bitter-jester-data-transfer/test-bitter-jester-data-transfer.zip"
-aws cloudformation deploy --template-file ./template.yml --stack-name "bitter-jester-data-manager-etl" --capabilities CAPABILITY_IAM --parameters "AWS_ACCESS_ID=$AWS_ACCESS_ID,AWS_SECRET_KEY=$AWS_SECRET_KEY,JOTFORM_API_KEY=$JOTFORM_API_KEY"
+aws cloudformation deploy --template-file ./template.yml --stack-name "bitter-jester-data-manager-etl" --capabilities CAPABILITY_IAM --parameter-overrides ParameterKey=AwsAccessID,ParameterValue=$AWS_ACCESS_ID ParameterKey=AwsSecretKey,ParameterValue=$AWS_SECRET_KEY ParameterKey=JotformApiKey,ParameterValue=$JOTFORM_API_KEY
