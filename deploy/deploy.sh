739ba9f29d7f89273743b97319436551f30f47a9
spencerkasper <spener.kasper@gmail.com>
2021-06-08 15:21:47 -0500

    * Since we are on 2.0, maybe this is the correct way to parameter override

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 rm -rf bitter-jester-data-manager-etl.zip
 zip -r bitter-jester-data-manager-etl.zip . -x '.idea/**' -x '.git/**'
 aws s3 cp ./bitter-jester-data-manager-etl.zip "s3://bitter-jester-test/bitter-jester-data-transfer/test-bitter-jester-data-transfer.zip"
-aws cloudformation deploy --template-file ./template.yml --stack-name "bitter-jester-data-manager-etl" --capabilities CAPABILITY_IAM --parameter-overrides ParameterKey=AwsAccessID,ParameterValue=${AWS_ACCESS_ID} ParameterKey=AwsSecretKey,ParameterValue=${AWS_SECRET_KEY} ParameterKey=JotformApiKey,ParameterValue=${JOTFORM_API_KEY}
+aws cloudformation deploy --template-file ./template.yml --stack-name "bitter-jester-data-manager-etl" --capabilities CAPABILITY_IAM --parameter-overrides AwsAccessID=${AWS_ACCESS_ID} AwsSecretKey=${AWS_SECRET_KEY} JotformApiKey=${JOTFORM_API_KEY}
