1caec93abeeac27a2df5dbdd35ebd6247c5c7b76
spencerkasper <spener.kasper@gmail.com>
2021-06-08 14:16:54 -0500

    * Handle passing environment variables to the cloudformation script and also delete the stack before deploying.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,5 @@
+aws cloudformation delete-stack --stack-name bitter-jester-data-manager-etl
 rm -rf bitter-jester-data-manager-etl.zip
 zip -r bitter-jester-data-manager-etl.zip . -x '.idea/**' -x '.git/**'
 aws s3 cp ./bitter-jester-data-manager-etl.zip "s3://bitter-jester-test/bitter-jester-data-transfer/test-bitter-jester-data-transfer.zip"
-aws cloudformation deploy --template-file ./template.yml --stack-name "bitter-jester-data-manager-etl" --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file ./template.yml --stack-name "bitter-jester-data-manager-etl" --capabilities CAPABILITY_IAM --parameters "AWS_ACCESS_ID=$AWS_ACCESS_ID,AWS_SECRET_KEY=$AWS_SECRET_KEY,JOTFORM_API_KEY=$JOTFORM_API_KEY"
