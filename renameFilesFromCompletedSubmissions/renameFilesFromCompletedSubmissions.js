7044f0ecfc35fd3279fa2f183607f4e3a834b29a
spencerkasper <spener.kasper@gmail.com>
2021-06-23 23:39:46 -0500

    * Try to create an archive of all the files that gets saved

--- renameFilesFromCompletedSubmissions/renameFilesFromCompletedSubmissions.js
+++ renameFilesFromCompletedSubmissions/renameFilesFromCompletedSubmissions.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const jotform = require('jotform');
+const archiver = require('archiver');
 const S3Client = require('../s3Client').S3Client;
 const extractAnswersFromJotform = require('../writeToS3FromJotForm/extractAnswersFromJotforrm');
 const axios = require("axios");
@@ -8,6 +9,7 @@ const JOTFORM_API_KEY = process.env.JOTFORM_API_KEY;
 const stream = require('stream');
 const util = require('util');
 const s3Client = new S3Client();
+const tmpFilePath = '/tmp/files/'
 
 jotform.options({
     debug: true,
@@ -26,6 +28,21 @@ async function downloadFile(fileUrl, outputLocationPath) {
     }).then(async response => {
         response.data.pipe(writer);
         return finished(writer);
+    });
+}
+
+function zipDirectory(source, out, fileName) {
+    const archive = archiver('zip', { zlib: { level: 9 }});
+    const stream = fs.createWriteStream(out);
+
+    return new Promise((resolve, reject) => {
+        archive
+            .directory(source, '/', {name: fileName})
+            .on('error', err => reject(err))
+            .pipe(stream);
+
+        stream.on('close', () => resolve());
+        archive.finalize();
     });
 }
 
@@ -48,12 +65,12 @@ async function getFormFiles(formId, competition) {
             console.error(extractedApplications);
             for (let app of extractedApplications) {
                 const fileNameFormattedBandName = app.bandName.trim().split(' ').join('-');
-
+                const filePathForBand = `${competition}/application-files/bandName=${fileNameFormattedBandName}/`;
                 for (let index = 0; index < app.bandLogoUrls.length; index++) {
                     const bandLogoUrl = app.bandLogoUrls[index];
                     const fileType = getFileType(bandLogoUrl);
                     const fullFileNameAfterRename = `${fileNameFormattedBandName}_Logo-${index + 1}.${fileType}`;
-                    const temporaryFilePath = `/tmp/${fullFileNameAfterRename}`;
+                    const temporaryFilePath = `${tmpFilePath}${fullFileNameAfterRename}`;
                     await downloadFile(bandLogoUrl, temporaryFilePath);
                     const s3FilePath = `${competition}/application-files/bandName=${fileNameFormattedBandName}/${fullFileNameAfterRename}`;
                     const contentType = fileType === 'jpeg' ? 'image/jpeg' : 'image/png';
@@ -72,9 +89,9 @@ async function getFormFiles(formId, competition) {
                     const bandPhotoUrl = app.bandPhotosUrls[index];
                     const fileType = getFileType(bandPhotoUrl);
                     const fileName = `${fileNameFormattedBandName}_Photo-${index + 1}.${fileType}`;
-                    const temporaryFilePath = `/tmp/${fileName}`;
+                    const temporaryFilePath = `${tmpFilePath}${fileName}`;
                     await downloadFile(bandPhotoUrl, temporaryFilePath);
-                    const s3FilePath = `${competition}/application-files/bandName=${fileNameFormattedBandName}/${fileName}`;
+                    const s3FilePath = `${filePathForBand}${fileName}`;
                     const contentType = `image/${fileType}`;
                     await s3Client.put(
                         s3Client.createPutPublicJsonRequest(
@@ -91,9 +108,9 @@ async function getFormFiles(formId, competition) {
                     const musicSamplesUrl = app.musicSamplesUrls[index];
                     const fileType = getFileType(musicSamplesUrl);
                     const fileName = `${fileNameFormattedBandName}_Music-${index + 1}.${fileType}`;
-                    const temporaryFilePath = `/tmp/${fileName}`;
+                    const temporaryFilePath = `${tmpFilePath}${fileName}`;
                     await downloadFile(musicSamplesUrl, temporaryFilePath);
-                    const s3FilePath = `${competition}/application-files/bandName=${fileNameFormattedBandName}/${fileName}`;
+                    const s3FilePath = `${filePathForBand}${fileName}`;
                     const contentType = fileType === 'mp3' ? 'audio/mpeg' : `audio/${fileType}`;
                     await s3Client.put(
                         s3Client.createPutPublicJsonRequest(
@@ -106,6 +123,14 @@ async function getFormFiles(formId, competition) {
                     console.log(`done with song ${s3FilePath}`)
                 }
             }
+            await zipDirectory(tmpFilePath, `/tmp/packaged.zip`);
+            await s3Client.put(
+                s3Client.createPutPublicJsonRequest(
+                    'bitter-jester-test',
+                    `${competition}/application-files/packaged.zip`,
+                    fs.readFileSync('/tmp/application-files.zip')
+                )
+            )
         })
         .fail(function (error) {
             console.log(`Error: ${error}`);
