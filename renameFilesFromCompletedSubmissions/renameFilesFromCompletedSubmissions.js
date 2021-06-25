63c9835b24b5cbeecd785173b53f745461f99ffa
spencerkasper <spener.kasper@gmail.com>
2021-06-25 17:07:43 -0500

    * Only download all of the files if the sns says so * Refactor a bit to condense the duplicated code * Change the shape of uploaded file object

--- renameFilesFromCompletedSubmissions/renameFilesFromCompletedSubmissions.js
+++ renameFilesFromCompletedSubmissions/renameFilesFromCompletedSubmissions.js
@@ -36,9 +36,42 @@ function getFileType(url) {
     return urlParts[urlParts.length - 1];
 }
 
+const getUploadedFile = (fileName, fileType, type, url, title = '') => {
+    return {
+        fileName,
+        fileType,
+        type,
+        url,
+        title
+    };
+}
 
-async function getFormFiles(formId, competition) {
+async function getFormFiles(formId, competition, shouldDownloadFiles) {
     const ALL_FILES_PATH = `${competition}/application-files/all/`;
+    async function downloadFromJotformAndWriteToS3(fullFileNameAfterRename, url, fileType, s3FilePath, contentType) {
+        if (shouldDownloadFiles) {
+            const temporaryFilePath = `${tmpFilePath}${fullFileNameAfterRename}`;
+            await downloadFile(url, temporaryFilePath);
+            await s3Client.put(
+                s3Client.createPutPublicJsonRequest(
+                    'bitter-jester-test',
+                    s3FilePath,
+                    fs.readFileSync(temporaryFilePath),
+                    contentType
+                )
+            );
+            await s3Client.put(
+                s3Client.createPutPublicJsonRequest(
+                    'bitter-jester-test',
+                    `${ALL_FILES_PATH}${fullFileNameAfterRename}`,
+                    fs.readFileSync(temporaryFilePath),
+                    contentType
+                )
+            );
+            fs.unlinkSync(temporaryFilePath);
+        }
+    }
+
     await jotform.getFormSubmissions(formId)
         .then(async function (applications) {
             const jotformAnswerMap = {
@@ -52,14 +85,10 @@ async function getFormFiles(formId, competition) {
                 bandName: '39',
             };
 
-            const extractedApplications = extractAnswersFromJotform.extractAnswersFromJotform(applications, jotformAnswerMap);
-            await s3Client.put(s3Client.createPutPublicJsonRequest(
-                'bitter-jester-test',
-                `${competition}/uploaded-files.json`,
-                JSON.stringify(extractedApplications),
-            ));
-            console.error(extractedApplications);
-            for (let app of extractedApplications) {
+            const uploadedFiles = extractAnswersFromJotform.extractAnswersFromJotform(applications, jotformAnswerMap);
+            console.error(uploadedFiles);
+            const allFiles = [];
+            for (let app of uploadedFiles) {
                 const fileNameFormattedBandName = app.bandName.trim().replace(/[^\w\s&]/gi, '').split(' ').join('-');
                 const filePathForBand = `${competition}/application-files/bandName=${fileNameFormattedBandName}/`;
                 for (let index = 0; index < app.bandLogoUrls.length; index++) {
@@ -67,27 +96,10 @@ async function getFormFiles(formId, competition) {
                         const bandLogoUrl = app.bandLogoUrls[index];
                         const fileType = getFileType(bandLogoUrl);
                         const fullFileNameAfterRename = `${fileNameFormattedBandName}_Logo-${index + 1}.${fileType}`;
-                        const temporaryFilePath = `${tmpFilePath}${fullFileNameAfterRename}`;
-                        await downloadFile(bandLogoUrl, temporaryFilePath);
                         const s3FilePath = `${competition}/application-files/bandName=${fileNameFormattedBandName}/${fullFileNameAfterRename}`;
                         const contentType = fileType === 'jpeg' ? 'image/jpeg' : 'image/png';
-                        await s3Client.put(
-                            s3Client.createPutPublicJsonRequest(
-                                'bitter-jester-test',
-                                s3FilePath,
-                                fs.readFileSync(temporaryFilePath),
-                                contentType
-                            )
-                        );
-                        await s3Client.put(
-                            s3Client.createPutPublicJsonRequest(
-                                'bitter-jester-test',
-                                `${ALL_FILES_PATH}${fullFileNameAfterRename}`,
-                                fs.readFileSync(temporaryFilePath),
-                                contentType
-                            )
-                        );
-                        fs.unlinkSync(temporaryFilePath);
+                        await downloadFromJotformAndWriteToS3(fullFileNameAfterRename, bandLogoUrl, fileType, s3FilePath, contentType);
+                        allFiles.push(getUploadedFile(fullFileNameAfterRename, fileType, 'logo', bandLogoUrl));
                         console.log(`done with logo ${s3FilePath}`)
                     } catch (e) {
                         console.error('Error with logo: ', e);
@@ -99,27 +111,9 @@ async function getFormFiles(formId, competition) {
                         const bandPhotoUrl = app.bandPhotosUrls[index];
                         const fileType = getFileType(bandPhotoUrl);
                         const fileName = `${fileNameFormattedBandName}_Photo-${index + 1}.${fileType}`;
-                        const temporaryFilePath = `${tmpFilePath}${fileName}`;
-                        await downloadFile(bandPhotoUrl, temporaryFilePath);
                         const s3FilePath = `${filePathForBand}${fileName}`;
-                        const contentType = `image/${fileType}`;
-                        await s3Client.put(
-                            s3Client.createPutPublicJsonRequest(
-                                'bitter-jester-test',
-                                s3FilePath,
-                                fs.readFileSync(temporaryFilePath),
-                                contentType
-                            )
-                        );
-                        await s3Client.put(
-                            s3Client.createPutPublicJsonRequest(
-                                'bitter-jester-test',
-                                `${ALL_FILES_PATH}${fileName}`,
-                                fs.readFileSync(temporaryFilePath),
-                                contentType
-                            )
-                        );
-                        fs.unlinkSync(temporaryFilePath);
+                        await downloadFromJotformAndWriteToS3(fileName, bandPhotoUrl, s3FilePath, `image/${fileType}`);
+                        allFiles.push(getUploadedFile(fileName, fileType, 'band_photo', bandPhotoUrl));
                         console.log(`done with photo ${s3FilePath}`);
                     } catch (e) {
                         console.error('Error with photo: ', e);
@@ -133,27 +127,10 @@ async function getFormFiles(formId, competition) {
                         const songName = index === 0 ? app.musicSampleTitle1 : app.musicSampleTitle2;
                         const formattedSongName = songName ? songName.trim().replace(/[^\w\s&]/gi, '').split(' ').join('-') : 'NO-NAME';
                         const fileName = `${fileNameFormattedBandName}_Music-${index + 1}_${formattedSongName}.${fileType}`;
-                        const temporaryFilePath = `${tmpFilePath}${fileName}`;
-                        await downloadFile(musicSamplesUrl, temporaryFilePath);
                         const s3FilePath = `${filePathForBand}${fileName}`;
                         const contentType = fileType === 'mp3' ? 'audio/mpeg' : `audio/${fileType}`;
-                        await s3Client.put(
-                            s3Client.createPutPublicJsonRequest(
-                                'bitter-jester-test',
-                                s3FilePath,
-                                fs.readFileSync(temporaryFilePath),
-                                contentType
-                            )
-                        );
-                        await s3Client.put(
-                            s3Client.createPutPublicJsonRequest(
-                                'bitter-jester-test',
-                                `${ALL_FILES_PATH}${fileName}`,
-                                fs.readFileSync(temporaryFilePath),
-                                contentType
-                            )
-                        );
-                        fs.unlinkSync(temporaryFilePath);
+                        await downloadFromJotformAndWriteToS3(fileName, musicSamplesUrl, fileType, s3FilePath, contentType)
+                        allFiles.push(getUploadedFile(fileName, fileType, 'music', musicSamplesUrl, songName));
                         console.log(`done with song ${s3FilePath}`)
                     } catch (e) {
                         console.error('Error with song: ', e);
@@ -165,32 +142,19 @@ async function getFormFiles(formId, competition) {
                         const stagePlotUrl = app.stagePlotUrls[index];
                         const fileType = getFileType(stagePlotUrl);
                         const fileName = `${fileNameFormattedBandName}_Stage-Plot-${index + 1}.${fileType}`;
-                        const temporaryFilePath = `${tmpFilePath}${fileName}`;
-                        await downloadFile(stagePlotUrl, temporaryFilePath);
                         const s3FilePath = `${filePathForBand}${fileName}`;
-                        const contentType = `image/${fileType}`;
-                        await s3Client.put(
-                            s3Client.createPutPublicJsonRequest(
-                                'bitter-jester-test',
-                                s3FilePath,
-                                fs.readFileSync(temporaryFilePath),
-                                contentType
-                            )
-                        );
-                        await s3Client.put(
-                            s3Client.createPutPublicJsonRequest(
-                                'bitter-jester-test',
-                                `${ALL_FILES_PATH}${fileName}`,
-                                fs.readFileSync(temporaryFilePath),
-                                contentType
-                            )
-                        );
-                        fs.unlinkSync(temporaryFilePath);
+                        await downloadFromJotformAndWriteToS3(fileName, fileType, stagePlotUrl, s3FilePath, `image/${fileType}`);
+                        allFiles.push(getUploadedFile(fileName, fileType, 'stage_plot', stagePlotUrl));
                         console.log(`done with stage plot ${s3FilePath}`)
                     } catch (e) {
                         console.error('Error with stage plot: ', e);
                     }
                 }
+                await s3Client.put(s3Client.createPutPublicJsonRequest(
+                    'bitter-jester-test',
+                    `${competition}/uploaded-files.json`,
+                    JSON.stringify({files: allFiles}),
+                ));
             }
         })
         .fail(function (error) {
--- renameFilesFromCompletedSubmissionsLambdaHandler.js
+++ renameFilesFromCompletedSubmissionsLambdaHandler.js
@@ -6,6 +6,9 @@ const BITTER_JESTER_COMPLETED_APPLICATIONS_JOTFORM_FORM_ID = 211443460736149;
 const OUTPUT_FILE_NAME = 'completed-submissions.json';
 
 exports.handler = async function (event) {
-    const competition = event.Records[0].Sns.Message;
-    await getFormFiles(BITTER_JESTER_COMPLETED_APPLICATIONS_JOTFORM_FORM_ID, competition);
+    const message = event.Records[0].Sns.Message;
+    const messageParts = message.split('&');
+    const competition = messageParts[0];
+    const shouldDownloadFiles = Boolean(messageParts[1]);
+    await getFormFiles(BITTER_JESTER_COMPLETED_APPLICATIONS_JOTFORM_FORM_ID, competition, shouldDownloadFiles);
 };
