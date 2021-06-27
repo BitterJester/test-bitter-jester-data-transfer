253dd0d49ae34e1dc65d66251bdb09c8f387a0f2
spencerkasper <spener.kasper@gmail.com>
2021-06-27 00:46:07 -0500

    * Debug

--- generateFridayNightBattleScheduleLambdaHandler.js
+++ generateFridayNightBattleScheduleLambdaHandler.js
@@ -9,6 +9,7 @@ exports.handler = async function (event, context) {
     const competition = event.Records[0].Sns.Message;
     const item = await s3Client.getObject(s3Bucket, `${competition}/completed-submissions.json`);
     const removedBands = await s3Client.getObject(s3Bucket, `${competition}/removed-bands.json`);
+    console.error(removedBands);
     const applications = item.completedApplications.filter(app => !removedBands.removedBands.includes(app.bandName));
     const schedule = await generateFridayNightBattleSchedule.generateFridayNightBattleSchedule(applications);
     const s3PutRequest = s3Client.createPutPublicJsonRequest(
