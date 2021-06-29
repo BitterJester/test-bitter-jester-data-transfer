5f7af0ec8b738d5f8ef697ba6439b03ed3a682c0
spencerkasper <spener.kasper@gmail.com>
2021-06-28 20:52:34 -0500

    * Update the completed submissions endpoint

--- generateFridayNightBattleScheduleLambdaHandler.js
+++ generateFridayNightBattleScheduleLambdaHandler.js
@@ -6,17 +6,21 @@ const s3Client = new S3Client();
 const s3Bucket = 'bitter-jester-test';
 
 exports.handler = async function (event, context) {
-    const competition = event.competition ? `competition=${event.competition}` : event.Records[0].Sns.Message;
-    const item = await s3Client.getObject(s3Bucket, `${competition}/completed-submissions.json`);
-    const removedBands = await s3Client.getObject(s3Bucket, `${competition}/removed-bands.json`);
-    console.error(removedBands);
-    const applications = item.completedApplications.filter(app => !removedBands.removedBands.includes(app.bandName));
-    const schedule = await generateFridayNightBattleSchedule.generateFridayNightBattleSchedule(applications);
-    const s3PutRequest = s3Client.createPutPublicJsonRequest(
-        s3Bucket,
-        `${competition}/friday-night-schedule.json`,
-        JSON.stringify(schedule)
-    );
-    await s3Client.put(s3PutRequest);
-    return schedule;
+    try {
+        const competition = event.competitionId ? `competition=${event.competitionId}` : event.Records[0].Sns.Message;
+        const item = await s3Client.getObject(s3Bucket, `${competition}/completed-submissions.json`);
+        const removedBands = await s3Client.getObject(s3Bucket, `${competition}/removed-bands.json`);
+        console.error(removedBands);
+        const applications = item.completedApplications.filter(app => !removedBands.removedBands.includes(app.bandName));
+        const schedule = await generateFridayNightBattleSchedule.generateFridayNightBattleSchedule(applications);
+        const s3PutRequest = s3Client.createPutPublicJsonRequest(
+            s3Bucket,
+            `${competition}/friday-night-schedule.json`,
+            JSON.stringify(schedule)
+        );
+        await s3Client.put(s3PutRequest);
+        return JSON.stringify({responseCode: 200, body: schedule});
+    } catch (e) {
+        return e;
+    }
 };
--- getCompletedSubmissionsLambdaHandler.js
+++ getCompletedSubmissionsLambdaHandler.js
@@ -9,17 +9,22 @@ const OUTPUT_FILE_NAME = 'completed-submissions.json';
 const snsClient = new SNSClient();
 
 exports.handler = async function (event) {
-    const competition = event.Records[0].Sns.Message;
-    await writeToS3FromJotForm.getFormSubmissions(
-        BITTER_JESTER_COMPLETED_APPLICATIONS_JOTFORM_FORM_ID,
-        `${competition}/${OUTPUT_FILE_NAME}`,
-        formatForS3.format
-    );
+    try {
+        const competition = event.competitionId? `competition=${event.competitionId}` : event.Records[0].Sns.Message;
+        const submissions = await writeToS3FromJotForm.getFormSubmissions(
+            BITTER_JESTER_COMPLETED_APPLICATIONS_JOTFORM_FORM_ID,
+            `${competition}/${OUTPUT_FILE_NAME}`,
+            formatForS3.format
+        );
 
-    const snsMessage = {
-        TopicArn: 'arn:aws:sns:us-east-1:771384749710:BandApplicationUpdatedSnsTopic',
-        Message: competition
-    };
+        const snsMessage = {
+            TopicArn: 'arn:aws:sns:us-east-1:771384749710:BandApplicationUpdatedSnsTopic',
+            Message: competition
+        };
 
-    await snsClient.publishSNSMessage(snsMessage);
+        await snsClient.publishSNSMessage(snsMessage);
+        return {responseCode: 200, body: submissions};
+    } catch (e) {
+        return e;
+    }
 };
