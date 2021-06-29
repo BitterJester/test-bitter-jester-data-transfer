5d432118376c77ef1af62efa5c58c0b5c74147c0
spencerkasper <spener.kasper@gmail.com>
2021-06-28 21:39:44 -0500

    * Try fixing completed submissions

--- getCompletedSubmissionsLambdaHandler.js
+++ getCompletedSubmissionsLambdaHandler.js
@@ -10,7 +10,7 @@ const snsClient = new SNSClient();
 
 exports.handler = async function (event) {
     try {
-        const competition = event.competitionId? `competition=${event.competitionId}` : event.Records[0].Sns.Message;
+        const competition = event.competitionId ? `competition=${event.competitionId}` : event.Records[0].Sns.Message;
         const submissions = await writeToS3FromJotForm.getFormSubmissions(
             BITTER_JESTER_COMPLETED_APPLICATIONS_JOTFORM_FORM_ID,
             `${competition}/${OUTPUT_FILE_NAME}`,
--- writeToS3FromJotForm/writeToS3FromJotForm.js
+++ writeToS3FromJotForm/writeToS3FromJotForm.js
@@ -17,20 +17,29 @@ async function getFormSubmissions(formId, filename, formatFunction) {
     const queryParams = {
         limit: 1000
     }
-    return jotform.getFormSubmissions(formId, queryParams)
-        .then(async function (response) {
-            const formattedResponse = await formatFunction(response, formId);
-            const s3PutRequest = s3Client.createPutPublicJsonRequest(
-                s3Bucket,
-                filename,
-                JSON.stringify(formattedResponse)
-            );
-            await s3Client.put(s3PutRequest);
-            return formattedResponse;
-        })
-        .fail(function (error) {
-            console.log(`Error: ${error}`);
-        });
+    const response = await jotform.getFormSubmissions(formId, queryParams);
+    const formattedResponse = await formatFunction(response, formId);
+    const s3PutRequest = s3Client.createPutPublicJsonRequest(
+        s3Bucket,
+        filename,
+        JSON.stringify(formattedResponse)
+    );
+    await s3Client.put(s3PutRequest);
+    return formattedResponse;
+    // return jotform.getFormSubmissions(formId, queryParams)
+    //     .then(async function (response) {
+    //         const formattedResponse = await formatFunction(response, formId);
+    //         const s3PutRequest = s3Client.createPutPublicJsonRequest(
+    //             s3Bucket,
+    //             filename,
+    //             JSON.stringify(formattedResponse)
+    //         );
+    //         await s3Client.put(s3PutRequest);
+    //         return formattedResponse;
+    //     })
+    //     .fail(function (error) {
+    //         console.log(`Error: ${error}`);
+    //     });
 }
 
 module.exports = {
