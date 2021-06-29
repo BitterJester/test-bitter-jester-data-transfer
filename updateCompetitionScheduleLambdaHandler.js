0d864535d86f70e5ce7eb070b7183a421b159216
spencerkasper <spener.kasper@gmail.com>
2021-06-28 19:58:26 -0500

    * Try and create an API gateway...

---
+++ updateCompetitionScheduleLambdaHandler.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const generateFridayNightBattleSchedule = require('./generateFridayNightBattleSchedule/generateFridayNightBattleSchedule');
+const S3Client = require('s3Client').S3Client;
+
+const s3Client = new S3Client();
+const s3Bucket = 'bitter-jester-test';
+
+exports.handler = async function (event, context) {
+    try{
+        const competition = `competition=${event.competitionId}`;
+        const item = await s3Client.getObject(s3Bucket, `${competition}/completed-submissions.json`);
+        const removedBands = await s3Client.getObject(s3Bucket, `${competition}/removed-bands.json`);
+        console.error(removedBands);
+        const applications = item.completedApplications.filter(app => !removedBands.removedBands.includes(app.bandName));
+        const schedule = await generateFridayNightBattleSchedule.generateFridayNightBattleSchedule(applications);
+        const s3PutRequest = s3Client.createPutPublicJsonRequest(
+            s3Bucket,
+            `${competition}/friday-night-schedule.json`,
+            JSON.stringify(schedule)
+        );
+        await s3Client.put(s3PutRequest);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(schedule)
+        }
+    } catch (e) {
+        return e;
+    }
+};
