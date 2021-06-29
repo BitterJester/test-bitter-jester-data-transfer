51751b25fc112dff9b03f55f15c0149a968703ec
spencerkasper <spener.kasper@gmail.com>
2021-06-28 20:31:23 -0500

    * Try merging the existing lambda with the api gateway

--- generateFridayNightBattleScheduleLambdaHandler.js
+++ generateFridayNightBattleScheduleLambdaHandler.js
@@ -6,7 +6,7 @@ const s3Client = new S3Client();
 const s3Bucket = 'bitter-jester-test';
 
 exports.handler = async function (event, context) {
-    const competition = event.Records[0].Sns.Message;
+    const competition = event.competition ? `competition=${event.competition}` : event.Records[0].Sns.Message;
     const item = await s3Client.getObject(s3Bucket, `${competition}/completed-submissions.json`);
     const removedBands = await s3Client.getObject(s3Bucket, `${competition}/removed-bands.json`);
     console.error(removedBands);
@@ -18,4 +18,5 @@ exports.handler = async function (event, context) {
         JSON.stringify(schedule)
     );
     await s3Client.put(s3PutRequest);
+    return schedule;
 };
