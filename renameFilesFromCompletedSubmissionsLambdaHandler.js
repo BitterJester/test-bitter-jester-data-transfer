5bc5997ba058896dfc3ddbab711f2e50741c8a92
spencerkasper <spener.kasper@gmail.com>
2021-06-30 19:20:26 -0500

    * Add an endpoint for renamed files

--- renameFilesFromCompletedSubmissionsLambdaHandler.js
+++ renameFilesFromCompletedSubmissionsLambdaHandler.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
-const formatForS3 = require('./completedApplications/formatForS3');
 const {getFormFiles} = require("./renameFilesFromCompletedSubmissions/renameFilesFromCompletedSubmissions");
 
 const BITTER_JESTER_COMPLETED_APPLICATIONS_JOTFORM_FORM_ID = 211443460736149;
-const OUTPUT_FILE_NAME = 'completed-submissions.json';
 
 exports.handler = async function (event) {
-    const message = event.Records[0].Sns.Message;
-    const messageParts = message.split('&');
-    const competition = messageParts[0];
-    const shouldDownloadFiles = Boolean(messageParts[1]);
+    let competition;
+    let shouldDownloadFiles = false;
+    if(event.Records){
+        const message = event.Records[0].Sns.Message;
+        const messageParts = message.split('&');
+        competition = messageParts[0];
+        shouldDownloadFiles = Boolean(messageParts[1]);
+    } else {
+        competition = `competition=${event.competitionId}`;
+    }
     await getFormFiles(BITTER_JESTER_COMPLETED_APPLICATIONS_JOTFORM_FORM_ID, competition, shouldDownloadFiles);
 };
